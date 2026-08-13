6e140110e87cf472a03f4ba17f3d5bc66e9a9f53
manivannancsmca <manivannancsmca@gmail.com>
2026-08-13 23:20:40 +0530

    AppConfig issue fixed

--- order-service/src/main/java/com/order_service/config/AppConfig.java
+++ order-service/src/main/java/com/order_service/config/AppConfig.java
@@ -7,10 +7,17 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class AppConfig {
 
+    // 1. Explicitly define the RestClient.Builder bean
+    @Bean
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
+
+    // 2. Inject that builder to create your custom RestClient bean
     @Bean
     public RestClient paymentRestClient(RestClient.Builder builder) {
         return builder
-                .baseUrl("http://localhost:8082")
+                .baseUrl("http://payment-service:8082")
                 .build();
     }
 }
